d4d06a22485a6ac2f302db66adc8813d2e419b8c
Rastanan17 <125308083+Rastanan17@users.noreply.github.com>
2026-08-05 15:12:23 -0300

    Add files via upload

---
+++ core/app/js/app.js
@@ -0,0 +1,64 @@
+// =======================================
+// UN REINO EN MÍRRAFEN
+// Archivo principal
+// =======================================
+
+// ---------------------------------------
+// Entrar al Reino
+// ---------------------------------------
+async function entrarAlReino(){
+
+    document.getElementById("introScreen").style.display = "none";
+
+    document.getElementById("reinoUI").style.display = "block";
+
+    const mainMenu = document.getElementById("mainMenu");
+
+    if(mainMenu){
+        mainMenu.style.display = "flex";
+    }
+
+    document.getElementById("content").innerHTML = "";
+
+    verificarNuevoDia();
+    revisarCalendario();
+    actualizarPerfil();
+    revisarCompensacion();
+
+    await cargarMisiones();
+    await cargarMapa();
+    await cargarRecompensas();
+    await cargarSistemaPergaminos();
+
+    mostrarMapaReino();
+    
+}
+
+
+// ---------------------------------------
+// Entrar con un aventurero
+// ---------------------------------------
+async function entrarPerfil(id){
+
+    localStorage.setItem("perfilActivo",id);
+
+    await entrarAlReino();
+
+}
+
+
+// ---------------------------------------
+// Inicio de la aplicación
+// ---------------------------------------
+window.addEventListener("DOMContentLoaded",()=>{
+
+    // Ocultar el Reino al iniciar
+
+    document.getElementById("reinoUI").style.display = "none";
+
+    // Primero mostrar logo
+
+    iniciarLogo();
+
+});
+
